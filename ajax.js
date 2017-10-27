c4a4d357092be8523f21abff12af1404c272a0ab
Lasharon Billups <l.billups92@gmail.com>
2017-10-27 00:55:28 +0000

    ajax

--- ajax.js
+++ ajax.js
@@ -1,1 +1,33 @@
-alert("findme");
+
+document.getElementById("loadbutton").addEventListener("click", ajaxcall);
+
+function ajaxcall(){
+   
+    var ajax = new XMLHttpRequest();
+    ajax.open("GET", "https://assets.breatheco.de/json/project_list.php", true);
+    ajax.addEventListener("load", ajaxArrives);
+    ajax.send();
+    
+
+}
+
+function ajaxArrives(event){
+    
+    var jsonObj = JSON.parse(event.target.response);
+    
+    console.log(jsonObj);
+    
+    
+        function myList(JSON) {
+            var a = "<ul>",
+                b = "</ul>",
+                c = [];
+            
+    for (var i = 0; i < JSON.length; i++) {
+         document.innerHTML( i + "---" + JSON[i].name );  
+    }
+            
+ }
+}
+
+
