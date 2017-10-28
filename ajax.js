86a124cd79b348f1966416c7917f79f268ad3bc8
Lasharon Billups <l.billups92@gmail.com>
2017-10-28 16:13:31 +0000

    complete ajax and add facebook login

--- ajax.js
+++ ajax.js
@@ -13,21 +13,47 @@ function ajaxcall(){
 
 function ajaxArrives(event){
     
+    var cardContent ='<div class="card" style="width: 20rem;">' +
+          '<img class="card-img-top" src="%img%" alt="Card image cap">' +
+          '<div class="card-body">'+
+            '<h4 class="card-title">%title%</h4>'+
+            '<p class="card-text">%desc%</p>'+
+          '</div>'+
+        '</div>';
+    
+         
+    
     var jsonObj = JSON.parse(event.target.response);
-    
-    console.log(jsonObj);
-    
-    
-        function myList(JSON) {
-            var a = "<ul>",
-                b = "</ul>",
-                c = [];
+       
+      var text ="";
+      var wholeContent = "";
+        
+        for (var i = 0 ; i <jsonObj.length; i++) {
             
-    for (var i = 0; i < JSON.length; i++) {
-         document.innerHTML( i + "---" + JSON[i].name );  
-    }
+            text = cardContent.replace("%img%", jsonObj[i].thumb);
+            text = text.replace("%title%", jsonObj[i].name);
+            text = text.replace("%desc%", jsonObj[i].description);
+            wholeContent += text;
             
- }
+            // text = "<li>" + jsonObj[i].name + "</li>";
+            // document.getElementById("getList").innerHTML += text;
+            
+            // text = "<li>" + jsonObj[i].thumb + "</li>";
+            // document.getElementById("getList").innerHTML += text;
+
+            // text = "<li>" + jsonObj[i].description + "</li>";
+            // document.getElementById("getList").innerHTML += text;
+            
+            // text = "<li>" + jsonObj[i].images + "</li>";
+            // document.getElementById("getList").innerHTML += text;
+            
+        }
+    
+    
+    var myJSON ="";
+    var localStorage ="";
+
+
+  document.getElementById("wholeContent").innerHTML=wholeContent;
+  
 }
-
-
